6130da2b19752e972b77378d6295b985360fc24c
Stephen Beard <srbeard@calpoly.edu>
2022-04-23 15:49:49 -0400

    Change port

--- 2224-csc430-test-handin/info.rkt
+++ 2224-csc430-test-handin/info.rkt
@@ -15,7 +15,7 @@
 ;; Modify the provided "icon.png" file to your school/lab/etc logo.
 
 ;; You must define a server:port for the client to be functional
-(define server:port "srbeard-office:80")
+(define server:port "srbeard-office:9100")
 
 ;; The following are optional. Uncomment and fill in
 ;; the values to add a menu item under "Help" to open
