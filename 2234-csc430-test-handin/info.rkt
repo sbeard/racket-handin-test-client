40ad2dc27e4d2b24b1e5e8f8a1bb422d804278ec
Stephen R. Beard <srbeard@calpoly.edu>
2023-04-23 13:18:02 -0700

    Update cert

--- 2234-csc430-test-handin/info.rkt
+++ 2234-csc430-test-handin/info.rkt
@@ -15,7 +15,7 @@
 ;; Modify the provided "icon.png" file to your school/lab/etc logo.
 
 ;; You must define a server:port for the client to be functional
-(define server:port "129.65.51.244:5900")
+(define server:port "129.65.51.244:9100")
 
 ;; The following are optional. Uncomment and fill in
 ;; the values to add a menu item under "Help" to open
